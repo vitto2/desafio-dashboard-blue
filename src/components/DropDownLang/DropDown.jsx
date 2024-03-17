674e6d623d7c95a687ba4876a787e7b77b583b43
Vitor <vitor2071@gmail.com>
2024-03-17 20:33:44 -0300

    feat: cria itens do header

--- src/components/DropDownLang/DropDown.jsx
+++ src/components/DropDownLang/DropDown.jsx
@@ -12,9 +12,7 @@ const DropDown = () => {
     <div className="idioma">
       <select name="idioma" id="idioma" value={idioma} onChange={handleChange}>
         <option value="pt-br">
-          <span className="pais">
-            <img src="" alt="" /> PT(BR)
-          </span>
+          <span className="pais">PT(BR)</span>
         </option>
         <option value="en-us">
           <span className="pais">EN(US)</span>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import "./style.css";
 import SearchBar from "./../SearchBar/SearchBar";
 import DropDown from "../DropDownLang/DropDown";
+import notificationIcon from "../../assets/notificationicon.svg";
+import avatarIcon from "../../assets/avatar-icon.jpg";
 
 const Header = () => {
   return (
@@ -13,6 +15,16 @@ const Header = () => {
       </div>
       <div>
         <DropDown />
+      </div>
+      <div className="notification-icon">
+        <img src={notificationIcon} alt="Icon de notificação" />
+      </div>
+      <div className="container-user-profile">
+        <img src={avatarIcon} alt="" />
+        <div>
+          <h3>Paulo Freire</h3>
+          <h5>Comercial</h5>
+        </div>
       </div>
     </header>
   );
