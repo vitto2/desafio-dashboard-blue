76e965ddae3ffb6c6c611518c012929660ea3303
Vitor <vitor2071@gmail.com>
2024-03-17 00:09:15 -0300

    feat: implementa card conheça a blue

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,21 +2,34 @@ import "./style.css";
 import logo from "../../assets/logo-blue.svg";
 import pizzaicon from "../../assets/pizza-graf-icon.svg";
 import leaderboardicon from "../../assets/leaderboard-icon.svg";
+import experienceicon from "../../assets/experience-icon.svg";
 
 const Aside = () => {
   return (
     <aside className="aside-main-container">
-      <div className="logo-text-section">
-        <img src={logo} alt="Logo da blue" />
-        <h2>Blue</h2>
+      <div>
+        <div className="logo-text-section">
+          <img src={logo} alt="Logo da blue" />
+          <h2>Blue</h2>
+        </div>
+        <div className="container-menu-dash">
+          <img src={pizzaicon} alt="" />
+          <h2>Dashboard</h2>
+        </div>
+        <div className="container-menu-leaderboard">
+          <img src={leaderboardicon} alt="" />
+          <h2>Leaderboard</h2>
+        </div>
       </div>
-      <div className="container-menu-dash">
-        <img src={pizzaicon} alt="" />
-        <h2>Dashboard</h2>
-      </div>
-      <div className="container-menu-leaderboard">
-        <img src={leaderboardicon} alt="" />
-        <h2>Leaderboard</h2>
+
+      <div className="container-card-experience">
+        <img className="xp-icon" src={experienceicon} alt="" />
+        <h3>Conheça a Blue</h3>
+        <p>
+          Saiba mais informações sobre a Blue, uma operadora de saúde Health
+          Tech
+        </p>
+        <button className="xp-btn">Conhecer</button>
       </div>
     </aside>
   );
