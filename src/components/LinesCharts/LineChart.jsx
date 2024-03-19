dae11912e663d1a3b3a07f818e0b07f5ffa9ea7e
vitto2 <vitor2071@gmail.com>
2024-03-19 17:39:22 -0300

    feat: cria gráfico de linha

---
+++ src/components/LinesCharts/LineChart.jsx
@@ -0,0 +1,56 @@
+import ApexChart from "react-apexcharts";
+
+const chart = () => {
+  const options = {
+    xaxis: {
+      type: "line",
+      categories: [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Abr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Set",
+        "Out",
+        "Nov",
+        "Dez",
+      ],
+    },
+    yaxis: {
+      tooltips: {
+        enabled: true,
+      },
+    },
+    title: {
+      text: "Plataformas",
+      align: "left",
+    },
+    series: [
+      {
+        name: "TD consultoria",
+        data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10],
+      },
+      {
+        name: "RIKO Plataforma",
+        data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
+      },
+      {
+        name: "Blue Serviços",
+        data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: true,
+      },
+    },
+  };
+  return <ApexChart options={options} series={options.series} />;
+};
+
+export default chart;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,18 @@
 import "./style.css";
 import Sales from "../Sales/Sales";
+import Chart from "./../LinesCharts/LineChart";
 
 const Main = () => {
   return (
     <main className="main-container">
-      <div>
-        <Sales />
+      <div className="main-container-div-01">
+        <div>
+          <Sales />
+        </div>
+        <div className="barChart">
+          <Chart className="chartLine" />
+          <chart />
+        </div>
       </div>
     </main>
   );
